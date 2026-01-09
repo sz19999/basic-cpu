e66681768e1c9e687bb2e0ae64903fb21e3600e4
sean <sean.zinger1999@gmail.com>
2026-01-09 17:23:14 +0200

    Finish control_unit.v
    added a missing fetch stage using an if else, then moved the
    decoding and executing phase into the else block.
    of course incremented the counter values in each case by one.

--- rtl/control_unit.v
+++ rtl/control_unit.v
@@ -20,13 +20,19 @@ localparam ALU_NOP = 2'b00;	// no operation
 localparam ALU_ADD = 2'b01;	// add
 localparam ALU_SUB = 2'b10;   // sub
 
+// OP codes
+localparam OP_NOP = 3'b000;
+localparam OP_MV  = 3'b001;
+localparam OP_ADD = 3'b010;
+localparam OP_SUB = 3'b011;
+localparam OP_MVI = 3'b100;
+
 // instruction fields
 wire [2:0] opcode, Rx, y;
 assign opcode = IR[8:6];
 assign     Rx = IR[5:3];
 assign      y = IR[2:0];
 
-// handles opcode
 always @(*) begin
 	// defaults outputs
    Rout   = 3'b000;
@@ -41,70 +47,87 @@ always @(*) begin
 	alu_op = ALU_NOP;
 	
 	if(run && resetn)
-		case(opcode)
-			3'b000: // nop
-					begin
-						clear = 1'b1;
-						done  = 1'b1;
-					end
+		// fetch
+		if (counter == 2'b00) begin
+			DINout = 1'b1;  // Bus <- Din
+			IRin   = 1'b1;  // IR  <- Bus
+			end
+		// decode + execute	
+		else begin
+			case(opcode)
+				OP_NOP:
+					if (counter == 2'b01)
+						begin
+							clear = 1'b1;
+							done  = 1'b1;
+						end
 				
-			3'b001: // mv
-				case(counter)
-					2'b00: begin
-						Rout = y;	 // Bus <- y (forward to mux for handling)
-						Ain  = 1'b0;
-						Gin  = 1'b0;
-						Rin  = (8'b1 << Rx);	// select register to load
-					end
-					2'b01: begin		// Rx <- y 
-						clear = 1'b1;	// reset counter
-						done  = 1'b1;	// signal done
-					end
-				endcase
+				OP_MV:
+					case(counter)
+						2'b01:
+							begin
+								Rout = y;	 			// Bus <- y (forward to mux for handling)
+								Rin  = (8'b1 << Rx);	// Rx  <- Bus
+							end
+						2'b10:
+							begin
+								clear = 1'b1;	// reset counter
+								done  = 1'b1;	// signal done
+							end
+					endcase
 				
-			3'b010:	// add
-				case(counter)
-					2'b00: begin
-						Rout = Rx;	 // Bus <- Rx
-						Ain  = 1'b1; // A   <- Rx
-					end
-					2'b01: begin		
-						Rout   = y;	 		// Bus <- Ry
-						alu_op = ALU_ADD; // A + Bus
-						Gin	 = 1'b1;		// G <- A + Bus
-					end
-					2'b10: begin		
-						Gout = 1'b1;			// Bus <- G
-						Rin  = (8'b1 << Rx); // select output register
-					end
-					2'b11: begin		
-						clear = 1'b1;
-						done  = 1'b1;
-					end
-				endcase
+				OP_ADD:
+					case(counter)
+						2'b01: begin
+							Rout = Rx;	 // Bus <- Rx
+							Ain  = 1'b1; // A   <- Bus
+						end
+						2'b10: begin		
+							Rout   = y;	 		// Bus <- Ry
+							alu_op = ALU_ADD; // A + Bus
+							Gin	 = 1'b1;		// G <- A + Bus
+						end
+						2'b11: begin		
+							Gout = 1'b1;			// Bus <- G
+							Rin  = (8'b1 << Rx); // select output register
+							clear = 1'b1;
+							done  = 1'b1;
+						end
+					endcase
 				
-			3'b011:	// sub
-				case(counter)
-					2'b00: begin
-						Rout = Rx;	 // Bus <- Rx
-						Ain  = 1'b1; // A   <- Rx
-					end
-					2'b01: begin		
-						Rout   = y;	 		// Bus <- Ry
-						alu_op = ALU_SUB; // A + Bus
-						Gin	 = 1'b1;		// G <- A + Bus
-					end
-					2'b10: begin		
-						Gout = 1'b1;			// Bus <- G
-						Rin  = (8'b1 << Rx); // select output register
-					end
-					2'b11: begin
-						clear = 1'b1;
-						done  = 1'b1;
-					end
-				endcase
-		
-		endcase
+				OP_SUB:
+					case(counter)
+						2'b01: begin
+							Rout = Rx;	 // Bus <- Rx
+							Ain  = 1'b1; // A   <- Bus
+						end
+						2'b10: begin		
+							Rout   = y;	 		// Bus <- Ry
+							alu_op = ALU_SUB; // A - Bus
+							Gin	 = 1'b1;		// G <- A - Bus
+						end
+						2'b11: begin		
+							Gout = 1'b1;			// Bus <- G
+							Rin  = (8'b1 << Rx); // Rx  <- Bus
+							clear = 1'b1;
+							done  = 1'b1;
+						end
+					endcase
+				
+				OP_MVI:
+					case(counter)
+						2'b01:
+							begin
+								DINout = 1'b1;			// Bus <- Din
+								Rin  = (8'b1 << Rx); // Rx  <- Bus
+							end
+						2'b10:
+							begin
+								clear = 1'b1;
+								done  = 1'b1;
+							end
+					endcase
+			endcase
 end
 
 endmodule
