1923aba1bd72646e40bf0c626fea86de4ed06d5d
sean <sean.zinger1999@gmail.com>
2026-01-16 12:46:19 +0200

    Add register.v testbench

---
+++ tb/register_tb.v
@@ -0,0 +1,42 @@
+
+`timescale 1ns/1ps
+
+module reg16_tb;
+
+reg clk, load;
+reg [15:0] vecin;
+wire [15:0] vecout;
+
+// setup clock 20ns
+initial begin
+	clk = 1'b0;
+	forever
+	#10 clk = ~clk;
+end
+
+initial begin
+	$display("clk | load | vecin | vecout");
+	$monitor("%b %b %b %b", clk, load, vecin, vecout);
+end
+
+// stimulus
+initial begin
+	#0 vecin = 16'b0000_0000_0000_1111;
+		load = 1'b0;
+	
+	#20 vecin = 16'b0000_0000_0000_1111;
+		 load = 1'b1;
+	
+	#20 vecin = 16'b0000_0000_0000_0000;
+	    load = 1'b1;
+	
+	#20 vecin = 16'b0000_0000_0000_1111;
+	    load = 1'b0;
+	
+	#20 $stop();
+end
+
+// uut instantation
+reg16 uut(clk, load, vecin, vecout);
+
+endmodule
