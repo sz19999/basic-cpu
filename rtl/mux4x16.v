c78edab922b2966a6817625ac77ea40763dce081
sean <sean.zinger1999@gmail.com>
2026-01-20 19:06:47 +0200

    Change the registers encoding R7, ..., R0
    prior to this update the registers were encoded in inverse order.
    this introduced bugs when selecting registers, e.g. selecting R0
    selected R7...
    inversing the encoding sovled the problem.

--- rtl/mux4x16.v
+++ rtl/mux4x16.v
@@ -16,14 +16,14 @@ always @(*) begin
 	case (select)
 		10'b00_0000_0001: bus = din;          								  // DIN
 		10'b00_0000_0010: bus = registers[word*k-1 : word*(k-1)]; 	  // G
-		10'b00_0000_0100: bus = registers[word*(k-1)-1 : word*(k-2)]; // R7
-      10'b00_0000_1000: bus = registers[word*(k-2)-1 : word*(k-3)]; // R6
-      10'b00_0001_0000: bus = registers[word*(k-3)-1 : word*(k-4)]; // R5
-      10'b00_0010_0000: bus = registers[word*(k-4)-1 : word*(k-5)]; // R4
-      10'b00_0100_0000: bus = registers[word*(k-5)-1 : word*(k-6)]; // R3
-      10'b00_1000_0000: bus = registers[word*(k-6)-1 : word*(k-7)]; // R2
-      10'b01_0000_0000: bus = registers[word*(k-7)-1 : word*(k-8)]; // R1
-      10'b10_0000_0000: bus = registers[word*(k-8)-1 : word*(k-9)]; // R0
+		10'b10_0000_0000: bus = registers[word*(k-1)-1 : word*(k-2)]; // R7
+      10'b01_0000_0000: bus = registers[word*(k-2)-1 : word*(k-3)]; // R6
+      10'b00_1000_0000: bus = registers[word*(k-3)-1 : word*(k-4)]; // R5
+      10'b00_0100_0000: bus = registers[word*(k-4)-1 : word*(k-5)]; // R4
+      10'b00_0010_0000: bus = registers[word*(k-5)-1 : word*(k-6)]; // R3
+      10'b00_0001_0000: bus = registers[word*(k-6)-1 : word*(k-7)]; // R2
+      10'b00_0000_1000: bus = registers[word*(k-7)-1 : word*(k-8)]; // R1
+      10'b00_0000_0100: bus = registers[word*(k-8)-1 : word*(k-9)]; // R0
       default: bus = {word{1'b0}};	// otherwise clear the bus
 	endcase
 end
