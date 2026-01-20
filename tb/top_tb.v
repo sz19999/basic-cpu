e8100e2aa972990a4058f2047f2d1283b5f80fa1
sean <sean.zinger1999@gmail.com>
2026-01-20 20:42:00 +0200

    Finish top_tb.v
    finished top level module testbench, tested and simulated it.

--- tb/top_tb.v
+++ tb/top_tb.v
@@ -10,6 +10,7 @@ reg clk, run, resetn;
 wire done;
 wire [word*r-1:0] registers;	// [R7, ..., R0]
 wire [word-1:0] bus_out;
+wire [1:0] counter_out;
 
 // OP codes
 localparam OP_NOP = 3'b000;
@@ -36,30 +37,111 @@ initial begin
 end
 
 initial begin
-	$display("clk | run | resetn | done | Din              | bus              | R7               | R6               | R5               | R4               | R3               | R2               | R1               | R0");
-	$monitor(" %b  | %b   | %b      | %b    | %b | %b | %b | %b | %b | %b | %b | %b | %b | %b", clk, run, resetn, 
+	$display("clk | run | resetn | done | Din              | bus              | R7               | R6               | R5               | R4               | R3               | R2               | R1               | R0               | counter");
+	$monitor(" %b  | %b   | %b      | %b    | %b | %b | %b | %b | %b | %b | %b | %b | %b | %b | %b", clk, run, resetn, 
 	done, Din, bus_out, 
 	registers[word*r-1:word*(r-1)], registers[word*(r-1)-1:word*(r-2)], registers[word*(r-2)-1:word*(r-3)],
 	registers[word*(r-3)-1:word*(r-4)], registers[word*(r-4)-1:word*(r-5)], registers[word*(r-5)-1:word*(r-6)], 
-	registers[word*(r-6)-1:word*(r-7)], registers[word*(r-7)-1:word*(r-8)]);
+	registers[word*(r-6)-1:word*(r-7)], registers[word*(r-7)-1:word*(r-8)], counter_out);
 end
 
 // stimulus
 initial begin
-	#0  run = 0; resetn = 0; 
+	#0  run = 0; resetn = 0; // reset system
 	
-	// IR = [III XXX YYY] = [opcode destination source]
 	// mvi R0, $6;
-	#20 run = 1; resetn = 1; 
-	    Din = {7'b0 ,OP_MVI, R0, 3'b0}; // instruction
-	#20 Din = 6; // data
-	#20 			 // let the operation finish  
+	@(posedge clk);					  // t0 - fetch
+	run = 1; resetn = 1; 			  // start
+	Din = {7'b0 ,OP_MVI, R0, 3'b0}; // IR = [III XXX YYY] = [opcode destination source]
+	@(posedge clk);					  // t1
+	Din = 6; 							  
+	@(posedge clk);					  // t2 - done
 	
-	#20 $stop();
+	// mvi R1, $1
+	@(posedge clk);	// t0
+	Din = {7'b0 ,OP_MVI, R1, 3'b0};
+	@(posedge clk);	// t1
+	Din = 1;
+	@(posedge clk);	// t2
+	
+	// add R1, R0 : R1 <- R1 + R0
+	@(posedge clk); // t0
+	Din = {7'b0 ,OP_ADD, R1, R0};
+	@(posedge clk); // t1
+	@(posedge clk); // t2
+	@(posedge clk); // t3 - done (the output register is updated on the next cycle)
+	
+	// mv R2, R1 : R2 <- R1
+	@(posedge clk); // t0
+	Din = {7'b0 ,OP_MV, R2, R1};
+	@(posedge clk); // t1
+	@(posedge clk); // t2 - done
+	
+	// mvi R3, $15
+	@(posedge clk);	// t0
+	Din = {7'b0 ,OP_MVI, R3, 3'b0};
+	@(posedge clk);	// t1
+	Din = 15;
+	@(posedge clk);	// t2
+	
+	// sub R3, R0 : R3 <- R3 - R0
+	@(posedge clk); // t0
+	Din = {7'b0 ,OP_SUB, R3, R0};
+	@(posedge clk); // t1
+	@(posedge clk); // t2
+	@(posedge clk); // t3 - done (the output register is updated on the next cycle)
+
+	// mv R4, R0
+	@(posedge clk); // t0
+	Din = {7'b0 ,OP_MV, R4, R0};
+	@(posedge clk); // t1
+	@(posedge clk); // t2 - done
+	
+	// sub R4, R1
+	@(posedge clk); // t0
+	Din = {7'b0 ,OP_SUB, R4, R1};
+	@(posedge clk); // t1
+	@(posedge clk); // t2
+	@(posedge clk); // t3 - done (the output register is updated on the next cycle)
+	
+	// mvi R5, $5
+	@(posedge clk);	// t0
+	Din = {7'b0 ,OP_MVI, R5, 3'b0};
+	@(posedge clk);	// t1
+	Din = 5;
+	@(posedge clk);	// t2 - done
+	
+	// mvi R6, $3
+	@(posedge clk);	// t0
+	Din = {7'b0 ,OP_MVI, R6, 3'b0};
+	@(posedge clk);	// t1
+	Din = 3;
+	@(posedge clk);	// t2 - done
+	
+	// add R6, R5
+	@(posedge clk); // t0
+	Din = {7'b0 ,OP_ADD, R6, R5};
+	@(posedge clk); // t1
+	@(posedge clk); // t2
+	@(posedge clk); // t3 - done
+	
+	// nop
+	@(posedge clk); // t0
+	Din = {7'b0 ,OP_NOP, 6'b0};
+	@(posedge clk); // t1 - done
+	
+	// mv R7, R6
+	@(posedge clk); // t0
+	Din = {7'b0 ,OP_MV, R7, R6};
+	@(posedge clk); // t1
+	@(posedge clk); // t2 - done
+	
+	@(posedge clk); 
+	$stop();
 	
 end
 
 // instantiate uut
-top uut(clk, Din, run, resetn, done, bus_out, registers);
+top uut(clk, Din, run, resetn, done, bus_out, registers, counter_out);
 
 endmodule 
